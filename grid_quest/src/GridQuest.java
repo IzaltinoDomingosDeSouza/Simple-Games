76784e1d53f328bd181e1d075fa02dd04a4a0954
IzaltinoDomingosDeSouza <izaltinodsouza@gmail.com>
2024-11-17 17:10:41 -0300

    Add try again

--- grid_quest/src/GridQuest.java
+++ grid_quest/src/GridQuest.java
@@ -10,26 +10,34 @@ public class GridQuest
     private static Random random = new Random();
     private static MapGrid map = new MapGrid(8,8);
     private static Player player = new Player(0,0);
+    private static UserInputOption input = new UserInputOption();
+    
     public static void main(String[] args)
     {
       new_game();
-      
-      System.out.println(player.get_posx() + " " + player.get_posy());
-      System.out.println(map.get_element(player.get_posx(),player.get_posy()).name() + "\n");
-      
-      // Debug
-      for (int y = 0; y < map.y_size(); y++)
-      {
-        for (int x = 0; x < map.x_size(); x++)
-        {
-          System.out.print(map.get_element(x,y).name() + " ");
-        }
-         System.out.println();
-      }
+      try_again();
     }
     public static void new_game()
     {
       map = MapLoader.load_from_disk("forest");
       player.set_pos(random.nextInt(map.x_size()), random.nextInt(map.y_size()));
     }
+    public static void try_again()
+    {
+      System.out.printf("Don't you want to try again?\n");
+      System.out.printf("[Y]es or [N]o : ");
+      
+      var yes_or_no = input.get_confirmation();
+      
+      switch(yes_or_no)
+      {
+        case Yes:
+          
+        break;
+        case No:
+          
+        break;
+      }
+    }
 }
+
