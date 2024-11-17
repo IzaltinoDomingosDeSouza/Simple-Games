83a46e8f38b02f14ae8d55b6f932636164db7b49
IzaltinoDomingosDeSouza <izaltinodsouza@gmail.com>
2024-11-17 16:52:22 -0300

    A simple refactor, to add support for a new game

--- grid_quest/src/GridQuest.java
+++ grid_quest/src/GridQuest.java
@@ -1,18 +1,18 @@
 import ids.izaltinodsouza.GridQuest.MapGrid;
 import ids.izaltinodsouza.GridQuest.MapLoader;
 import ids.izaltinodsouza.GridQuest.Player;
+import ids.izaltinodsouza.GridQuest.UserInputOption;
 
 import java.util.Random;
 
 public class GridQuest
 {
+    private static Random random = new Random();
+    private static MapGrid map = new MapGrid(8,8);
+    private static Player player = new Player(0,0);
     public static void main(String[] args)
     {
-      MapLoader map_loader = new MapLoader();
-      MapGrid map = map_loader.load_from_disk("forest");
-      
-      Random random = new Random();
-      Player player = new Player(random.nextInt(map.x_size()), random.nextInt(map.y_size()));
+      new_game();
       
       System.out.println(player.get_posx() + " " + player.get_posy());
       System.out.println(map.get_element(player.get_posx(),player.get_posy()).name() + "\n");
@@ -26,5 +26,10 @@ public class GridQuest
         }
          System.out.println();
       }
+    }
+    public static void new_game()
+    {
+      map = MapLoader.load_from_disk("forest");
+      player.set_pos(random.nextInt(map.x_size()), random.nextInt(map.y_size()));
     }
 }
